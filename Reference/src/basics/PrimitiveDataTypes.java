d37af0c8802961b4debb44e7f1c89380b0ae67cb
shawn <shawn@10.0.0.212>
2022-09-17 16:46:22 -0400

    Initial commit

---
+++ Reference/src/basics/PrimitiveDataTypes.java
@@ -0,0 +1,54 @@
+package basics;
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		/*
+		 * Words/Characters
+		 */
+		//Create a string variable called hello
+		//String variables hold an entire "string" of letters
+		String hello = "Hi there!";
+		//char variables hold 1 character such as a letter or special character (!@#%)
+		//char variables, unlike strings, use single quotes
+		char favoriteLetter = 's';
+		
+		/*
+		 * Numbers
+		 */
+		//The smallest number type allowing whole numbers ranging from -128 to 127
+		//1 byte size
+		byte linesOfCode = 48;
+		//A short allows whole numbers ranging from -32,768 to 32,767
+		//2 bytes size
+		short currentYear = 2022;
+		//Most commonly used to store numbers, allows whole numbers ranging from -2,147,483,648 to 2,147,483,647
+		//4 bytes size
+		int furthestDigged = -12262;
+		//The longest whole number only data type, allows whole numbers ranging from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		long earthSurfaceArea = 510100000;
+		//float - a floating-point number which stores a decimal value with 6 digits of precision
+		float conversionRate = 1.3266441f;
+		//double - a floating-point number which stores decimal values with 15 digits of precision
+		double doubleEx = 1.192648552817245;
+		
+		/*
+		 * Others
+		 */
+		//boolean - stores either a true or false value
+		boolean isRunning = true;
+		
+		/*
+		 * Example output
+		 */
+		if (isRunning == true) {
+			System.out.println(hello);
+			System.out.println("My favorite letter is " + favoriteLetter);
+			System.out.print("This program takes up " + linesOfCode + " lines of code,");
+			System.out.print(" and was completed in the year " + currentYear);
+			System.out.println("As of now, the furthest underground that humans have digged is" + furthestDigged + "m");
+			System.out.println("A quick fact to leave you with is the surface area of Earth is " + earthSurfaceArea + " km^2");
+			System.out.println("As of the date of publishing, the conversion rate of 1USD equals " + conversionRate + " CAD");
+			System.out.println("A double allows for 15 digits of decimal percision, here's an example: " + doubleEx);
+		}
+	}
+}
